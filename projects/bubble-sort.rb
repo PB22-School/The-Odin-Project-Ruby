41582060026baead90cd5eb5768da52cc3cf2031
PB22-School <nuhuh>
2025-03-27 09:21:08 -0400

    bubble sort both ways

--- projects/bubble-sort.rb
+++ projects/bubble-sort.rb
@@ -1,7 +1,7 @@
 
-def is_sorted?(array)
+def is_sorted?(array, least_to_greatest = true)
   0.upto(array.length - 2) do |i|
-    if array[i] > array[i + 1]
+    if (least_to_greatest ? array[i]: -array[i]) > (least_to_greatest ? array[i + 1]: -array[i + 1])
       return false
     end
   end
@@ -13,14 +13,14 @@ def bubble_sort(array, least_to_greatest = true)
   
   loop do
     0.upto(array.length - 2) do |i|
-      if array[i] > array[i + 1]
+      if (least_to_greatest ? array[i]: -array[i]) > (least_to_greatest ? array[i + 1]: -array[i + 1])
         temp = array[i + 1]
         array[i + 1] = array[i]
         array[i] = temp
       end
     end
 
-    if is_sorted?(array)
+    if is_sorted?(array, least_to_greatest)
       break
     end
 
@@ -30,4 +30,4 @@ def bubble_sort(array, least_to_greatest = true)
 
 end
 
-puts bubble_sort([4,3,78,2,0,2])
+puts bubble_sort([4,3,78,2,0,2], false)
